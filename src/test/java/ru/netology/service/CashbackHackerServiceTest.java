9fe971c1826adfd0dbb6369997229ad4f4228bb5
zdorovilov test <zdorovilovde@mail.ru>
2024-07-22 17:49:16 +0500

    Реализованы автотесты

--- src/test/java/ru/netology/service/CashbackHackerServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackerServiceTest.java
@@ -1,4 +1,26 @@
 package ru.netology.service;
 
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
 public class CashbackHackerServiceTest {
+
+    @org.testng.annotations.Test
+    public void testRemain(){
+        CashbackHackerService service = new CashbackHackerService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testRemainWithCashback(){
+        CashbackHackerService service = new CashbackHackerService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(actual, expected);
+    }
 }
